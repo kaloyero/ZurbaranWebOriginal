d32be2761bb75b0b3f5a1955d73533ddae0d11a7
Jandro <kaloye_ale@hotmail.com>
2014-04-06 20:20:46 -0300

    borro clases

--- src/main/java/com/contable/hibernate/dao/DocumentoTipoConceptoDao.java
+++
@@ -1,9 +0,0 @@
-package com.contable.hibernate.dao;
-
-import com.contable.common.GenericDao;
-import com.contable.hibernate.model.TipoDocumentoConcepto;
-
-public interface DocumentoTipoConceptoDao extends GenericDao<TipoDocumentoConcepto, Integer> {
-
-
-}
--- src/main/java/com/contable/hibernate/dao/DocumentoValorTerceIngre_VDao.java
+++
@@ -1,9 +0,0 @@
-package com.contable.hibernate.dao;
-
-import com.contable.common.GenericDao;
-import com.contable.hibernate.model.DocumentoValorTerceIngre_V;
-
-public interface DocumentoValorTerceIngre_VDao extends GenericDao<DocumentoValorTerceIngre_V, Integer> {
-
-
-}
