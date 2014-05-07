d78f959ba2c606a1fa1956b0ddb7f54d9cae121b
Jandro <kaloye_ale@hotmail.com>
2014-05-07 19:32:40 -0300

    Ultimo numero amigo

--- src/main/java/com/contable/hibernate/dao/impl/NumeracionDaoImpl.java
+++ src/main/java/com/contable/hibernate/dao/impl/NumeracionDaoImpl.java
@@ -1,5 +1,6 @@
 package com.contable.hibernate.dao.impl;
 
+import org.apache.commons.lang.StringUtils;
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
@@ -37,12 +38,31 @@ public class NumeracionDaoImpl extends GenericDaoImpl<Numeracion, Integer> imple
        
  	   criteria.add(Restrictions.eq("tipoDocumentoId", idTipoDocumento));
  	   criteria.add(Restrictions.eq("administracionId", idAdministracion));
- 	   criteria.add(Restrictions.eq("numeroLetra", letra));
- 	   criteria.add(Restrictions.eq("numeroEstablecimiento", establecimiento));
- 	   criteria.add(Restrictions.eq("numeroAnio", numAnio));
- 	   criteria.add(Restrictions.eq("numeroMes", numMes));
- 	   criteria.add(Restrictions.eq("numeroDia", numDia));
- 	 
+ 	   	if (StringUtils.isNotBlank(letra)){
+ 		  criteria.add(Restrictions.like("numeroLetra", letra));   
+ 	   	} else {
+ 		  criteria.add(Restrictions.isNull("numeroLetra"));
+ 	   	}
+ 	   	if (establecimiento != null && establecimiento > 0){
+ 	   		criteria.add(Restrictions.eq("numeroEstablecimiento", establecimiento));   
+ 	   	} else {
+ 	   		criteria.add(Restrictions.isNull("numeroEstablecimiento"));
+ 	   	}
+		if (numAnio != null && numAnio > 0){
+			criteria.add(Restrictions.eq("numeroAnio", numAnio));
+ 	   	} else {
+ 	   		criteria.add(Restrictions.isNull("numeroAnio"));
+ 	   	}
+		if (numMes != null && numMes > 0){
+			criteria.add(Restrictions.eq("numeroMes", numMes));
+ 	   	} else {
+ 	   		criteria.add(Restrictions.isNull("numeroMes"));
+ 	   	}
+		if (numDia != null && numDia > 0){
+			criteria.add(Restrictions.eq("numeroDia", numDia));
+ 	   	} else {
+ 	   		criteria.add(Restrictions.isNull("numeroDia"));
+ 	   	}
        
        return  (Numeracion) criteria.uniqueResult();
 
