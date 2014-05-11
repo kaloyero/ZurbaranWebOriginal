e799db614244bf5394686cc359107706dbfdea7e
alexisjk <alexis.abdel@glooobal.com>
2014-05-11 17:02:52 -0300

    Arrrrr

--- src/main/webapp/resources/js/uiClases/DocumentoListado.js
+++ src/main/webapp/resources/js/uiClases/DocumentoListado.js
@@ -2,7 +2,7 @@ var DocumentoListado = new Class({
 	Extends : Render,
 	initialize : function(name) {
 		this.name = name;
-		this.type = "documentoListado";
+		this.type = "documento";
 		this.breadcrumb = 'Documentos';
 		this.descripcion = "Desde aqui gestiones los Documentos";
 	},
