b6117c6228fd17bc94bae7cfaac4d66ddaf3b52b
alexisjk <alexis.abdel@glooobal.com>
2014-07-17 13:38:22 -0300

    Cosas Chequera

--- src/main/java/com/contable/controllers/ChequeraController.java
+++ src/main/java/com/contable/controllers/ChequeraController.java
@@ -16,13 +16,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.contable.common.ConfigurationControllerImpl;
 import com.contable.common.ConfigurationManager;
 import com.contable.common.beans.ConfigBean;
+import com.contable.common.beans.FiltroValPropiosBean;
 import com.contable.common.constants.Constants;
 import com.contable.common.utils.ConvertionUtil;
 import com.contable.form.ChequeraForm;
+import com.contable.form.ValorPropioForm;
 import com.contable.hibernate.model.Chequera;
 import com.contable.manager.AdministracionManager;
 import com.contable.manager.ChequeraManager;
 import com.contable.manager.CuentaManager;
+import com.contable.manager.DocumentoPropioManager;
 import com.contable.manager.EntidadManager;
 import com.contable.manager.MonedaManager;
 import com.contable.manager.TipoEntidadManager;
@@ -50,6 +53,9 @@ public class ChequeraController  extends ConfigurationControllerImpl<Chequera, C
 	@Autowired
 	private CuentaManager cuentaManager;
 
+	@Autowired
+	private DocumentoPropioManager documentoPropioManager;
+
 	@Override
 	protected ConfigurationManager<Chequera, ChequeraForm> getRelatedManager() {
 		return chequeraManager;
@@ -62,7 +68,7 @@ public class ChequeraController  extends ConfigurationControllerImpl<Chequera, C
 		row.add(formRow.getAdministracionNombre());
 		row.add(ConvertionUtil.StrValueOf(formRow.getNumeroIni()));
 		row.add(ConvertionUtil.StrValueOf(formRow.getNumeroFin()));
-		row.add("<a href='#' class='contView'><img style='width:20px;height:20;display:inline;float:right;margin-top:0.1cm;' src='resources/images/view.jpg'></a>");
+		row.add("<a href='#' class='contNoDisponible'><img style='width:20px;height:20;display:inline;float:right;margin-top:0.1cm;' src='resources/images/view.jpg'></a><a href='#' class='contAddNoDisponible'><img style='width:20px;height:20;display:inline;float:right;margin-top:0.1cm;' src='resources/images/view.jpg'></a><a href='#' class='contView'><img style='width:20px;height:20;display:inline;float:right;margin-top:0.1cm;' src='resources/images/view.jpg'></a>");
 
 		return row;
 	}
@@ -88,7 +94,10 @@ public class ChequeraController  extends ConfigurationControllerImpl<Chequera, C
 	@RequestMapping(value = "/getEntidadById/{id}", method = RequestMethod.GET)
 	public String get(Locale locale, Model model,@PathVariable int id, HttpServletRequest request) throws ParseException{
 		ChequeraForm chequera =chequeraManager.findById(id);
+		FiltroValPropiosBean filtro =new FiltroValPropiosBean();
+		//List<ValorPropioForm> listado = documentoPropioManager.buscarPorFiltros(filtro, "mostrar anulados", true);
 
+		//model.addAttribute("test", listado);
 		model.addAttribute("Chequera", chequera);
 	
 		return "configuraciones/editChequera";
